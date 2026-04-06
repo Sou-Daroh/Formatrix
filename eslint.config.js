f3db3eed33696e9db2acb1e6bddb23bdf140ad4b
Daroh Sou <daroh.sou22@kit.edu.kh>
2026-04-06 10:46:18 +0700

    chore: add eslint 9 flat config for svelte and typescript

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import globals from "globals";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import svelteParser from "svelte-eslint-parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["src/**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      svelte,
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      ...svelte.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  {
+    ignores: ["dist/", "src-tauri/", "node_modules/", ".svelte-kit/"],
+  },
+];
