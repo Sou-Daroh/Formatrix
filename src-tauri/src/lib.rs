c00c626ca237ab31b95864117b6a549c8354a947
Daroh Sou <daroh.sou22@kit.edu.kh>
2026-04-07 23:23:18 +0700

    register native clipboard plugin and grant the os capability permissions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ pub mod processor;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_next::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
